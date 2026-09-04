c470a81147c522522ae9eb900e723c6907f9429a
flopdy <285796692+flopdy@users.noreply.github.com>
2026-09-05 00:20:48 +0100

    change building tools move efi bin and build bash script into output

---
+++ output/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/repos/boot/edk2/
+./edksetup.sh
+build
+cp ~/repos/boot/edk2/Build/MdeModule/DEBUG_GCC/X64/MdeModulePkg/Application/myOs/myOs/OUTPUT/myOs.efi ~/repos/boot/output/BOOTX64.EFI
+
+cd ~/repos/boot/output/
+ls
