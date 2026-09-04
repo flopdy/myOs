70c53e9fa64a73277fb5670adef6f0bb69a7fc56
flopdy <285796692+flopdy@users.noreply.github.com>
2026-09-05 00:39:42 +0100

    change build.sh remove run source edksetup.sh

--- output/build.sh
+++ output/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd ~/repos/boot/edk2/
-./edksetup.sh
 build
 cp ~/repos/boot/edk2/Build/MdeModule/DEBUG_GCC/X64/MdeModulePkg/Application/myOs/myOs/OUTPUT/myOs.efi ~/repos/boot/output/BOOTX64.EFI
 
